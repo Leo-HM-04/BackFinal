2b58264a9bc75d2dd06d181dccd9706a6552bbe8
KikeGonRam <kikeramirez160418@gmail.com>
2025-07-14 16:28:32 -0600

    BASE ACTUALIZADA

--- plataforma_solicitudes_pago.sql
+++ plataforma_solicitudes_pago.sql
@@ -1,27 +1,9 @@
--- --------------------------------------------------------
-
--- Estructura de tabla para la tabla comprobantes_pago
-
-CREATE TABLE `comprobantes_pago` (
-  `id_comprobante` int(11) NOT NULL AUTO_INCREMENT,
-  `id_solicitud` int(11) NOT NULL,
-  `nombre_archivo` varchar(255) NOT NULL,
-  `ruta_archivo` varchar(255) NOT NULL,
-  `fecha_subida` datetime DEFAULT current_timestamp(),
-  `usuario_subio` int(11) NOT NULL,
-  `comentario` text DEFAULT NULL,
-  PRIMARY KEY (`id_comprobante`),
-  KEY `id_solicitud` (`id_solicitud`),
-  KEY `usuario_subio` (`usuario_subio`),
-  CONSTRAINT `fk_comprobante_solicitud` FOREIGN KEY (`id_solicitud`) REFERENCES `solicitudes_pago` (`id_solicitud`),
-  CONSTRAINT `fk_comprobante_usuario` FOREIGN KEY (`usuario_subio`) REFERENCES `usuarios` (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-07-2025 a las 08:54:13
+-- Tiempo de generación: 15-07-2025 a las 00:26:25
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -38,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `plataforma_solicitudes_pago`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comprobantes_pago`
+--
+
+CREATE TABLE `comprobantes_pago` (
+  `id_comprobante` int(11) NOT NULL,
+  `id_solicitud` int(11) NOT NULL,
+  `nombre_archivo` varchar(255) NOT NULL,
+  `ruta_archivo` varchar(255) NOT NULL,
+  `fecha_subida` datetime DEFAULT current_timestamp(),
+  `usuario_subio` int(11) NOT NULL,
+  `comentario` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `comprobantes_pago`
+--
+
+INSERT INTO `comprobantes_pago` (`id_comprobante`, `id_solicitud`, `nombre_archivo`, `ruta_archivo`, `fecha_subida`, `usuario_subio`, `comentario`) VALUES
+(1, 14, 'Captura de pantalla 2025-07-14 135016.png', 'uploads\\facturas\\1752528568478-982446442.png', '2025-07-14 15:29:28', 24, NULL),
+(2, 11, 'Captura de pantalla 2025-07-14 135016.png', 'uploads\\facturas\\1752528605251-279067567.png', '2025-07-14 15:30:05', 24, NULL),
+(3, 11, 'Captura de pantalla 2025-07-14 134309.png', 'uploads\\comprobantes\\1752528787220-134358371.png', '2025-07-14 15:33:07', 24, NULL),
+(4, 12, 'Captura de pantalla 2025-07-14 154244.png', 'uploads\\comprobantes\\1752529407173-514779375.png', '2025-07-14 15:43:27', 24, NULL),
+(5, 18, 'A_Sample_PDF__1_.pdf', 'uploads\\comprobantes\\1752529750537-751460955.pdf', '2025-07-14 15:49:10', 24, NULL);
 
 -- --------------------------------------------------------
 
@@ -83,32 +92,22 @@ INSERT INTO `notificaciones` (`id_notificacion`, `id_usuario`, `mensaje`, `leida
 (13, 5, '📥 Nueva solicitud pendiente de aprobación.', 0, '2025-07-10 13:52:14'),
 (14, 5, '📥 Nueva solicitud pendiente de aprobación.', 0, '2025-07-10 13:54:44'),
 (15, 5, '📥 Nueva solicitud pendiente de aprobación.', 0, '2025-07-10 15:10:01'),
-(16, 5, '📥 Nueva solicitud pendiente de aprobación.', 0, '2025-07-10 21:03:03'),
-(17, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 21:06:46'),
-(18, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 21:06:47'),
-(19, 20, '❌ Tu solicitud fue rechazada.', 0, '2025-07-10 21:06:51'),
-(20, 20, '💸 Tu solicitud ha sido pagada.', 0, '2025-07-10 22:53:24'),
-(21, 5, '💸 Se pagó la solicitud que aprobaste.', 0, '2025-07-10 22:53:30'),
-(22, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 23:01:55'),
-(23, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 23:01:58'),
-(24, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 23:01:59'),
-(25, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 23:02:00'),
-(26, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 23:02:01'),
-(27, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 23:02:01'),
-(28, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 23:02:01'),
-(29, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 23:02:01'),
-(30, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 23:02:02'),
-(31, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 23:02:02'),
-(32, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 23:02:02'),
-(33, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 23:02:03'),
-(34, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 23:02:04'),
-(35, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 23:02:06'),
-(36, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-10 23:02:17'),
-(37, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-10 23:02:18'),
-(38, 20, '💸 Tu solicitud ha sido pagada.', 0, '2025-07-10 23:22:43'),
-(39, 5, '💸 Se pagó la solicitud que aprobaste.', 0, '2025-07-10 23:22:48'),
-(40, 20, '💸 Tu solicitud ha sido pagada.', 0, '2025-07-10 23:39:24'),
-(41, 5, '💸 Se pagó la solicitud que aprobaste.', 0, '2025-07-10 23:39:30');
+(16, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-11 10:23:09'),
+(17, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-11 10:23:11'),
+(18, 25, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-11 10:23:12'),
+(19, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-11 10:23:17'),
+(20, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-11 10:23:18'),
+(21, 25, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-11 10:23:19'),
+(22, 20, '💸 Tu solicitud ha sido pagada.', 0, '2025-07-11 10:24:09'),
+(23, 23, '💸 Se pagó la solicitud que aprobaste.', 0, '2025-07-11 10:24:10'),
+(24, 20, '✅ Tu solicitud fue autorizada.', 0, '2025-07-11 11:05:20'),
+(25, 6, '📝 Nueva solicitud autorizada para pago.', 0, '2025-07-11 11:05:21'),
+(26, 20, '💸 Tu solicitud ha sido pagada.', 0, '2025-07-11 11:08:37'),
+(27, 25, '💸 Se pagó la solicitud que aprobaste.', 0, '2025-07-11 11:08:37'),
+(28, 20, '💸 Tu solicitud ha sido pagada.', 0, '2025-07-11 11:17:09'),
+(29, 23, '💸 Se pagó la solicitud que aprobaste.', 0, '2025-07-11 11:17:09'),
+(30, 20, '💸 Tu solicitud ha sido pagada.', 0, '2025-07-11 11:26:49'),
+(31, 5, '💸 Se pagó la solicitud que aprobaste.', 0, '2025-07-11 11:26:49');
 
 -- --------------------------------------------------------
 
@@ -161,15 +160,14 @@ CREATE TABLE `solicitudes_pago` (
 --
 
 INSERT INTO `solicitudes_pago` (`id_solicitud`, `id_usuario`, `departamento`, `monto`, `cuenta_destino`, `factura_url`, `concepto`, `tipo_pago`, `fecha_limite_pago`, `estado`, `comentario_aprobador`, `fecha_creacion`, `id_aprobador`, `id_pagador`, `fecha_revision`, `fecha_pago`, `id_recurrente_origen`) VALUES
-(11, 20, 'cobranza', 12342.00, '12345678907', '/uploads/facturas/1752171068008-464643402.png', 'prueba ', 'efectivo', '2025-08-09', 'pagada', 'Solicitud aprobada', '2025-07-10 12:11:08', 5, 6, '2025-07-10 21:06:46', '2025-07-10 22:53:24', NULL),
-(12, 20, 'automatizaciones', 42.00, '3454765', '/uploads/facturas/1752171426628-537657791.pdf', 'XDDD', 'efectivo', '2025-07-23', 'pendiente', NULL, '2025-07-10 12:17:06', NULL, NULL, NULL, NULL, NULL),
+(11, 20, 'cobranza', 12342.00, '12345678907', '/uploads/facturas/1752171068008-464643402.png', 'prueba ', 'efectivo', '2025-08-09', 'pagada', 'Solicitud aprobada', '2025-07-10 12:11:08', 25, 22, '2025-07-11 11:05:20', '2025-07-11 11:08:37', NULL),
+(12, 20, 'automatizaciones', 42.00, '3454765', '/uploads/facturas/1752171426628-537657791.pdf', 'XDDD', 'efectivo', '2025-07-23', 'pagada', 'Solicitud aprobada', '2025-07-10 12:17:06', 23, 25, '2025-07-11 10:23:17', '2025-07-11 10:24:09', NULL),
 (13, 20, 'automatizaciones', 1000.00, '1234567890', '/uploads/facturas/1752173100147-909676920.pdf', 'PRUEBA ', 'efectivo', '2025-08-09', 'pendiente', NULL, '2025-07-10 12:45:00', NULL, NULL, NULL, NULL, NULL),
-(14, 20, 'ti', 2222222.00, '1234567890', '/uploads/facturas/1752173198561-209150711.png', '98765432', '', '2025-08-09', 'autorizada', 'Solicitud rechazada', '2025-07-10 12:46:38', 5, NULL, '2025-07-10 21:06:51', NULL, NULL),
-(15, 20, 'vinculacion', 10033.00, '1234567890', '/uploads/facturas/1752173900916-983062405.png', 'test ', 'efectivo', '2025-08-09', 'pendiente', NULL, '2025-07-10 12:58:20', NULL, NULL, NULL, NULL, NULL),
-(17, 20, 'atencion a clientes', 1223344.00, '1234567890', '/uploads/facturas/1752176713500-269532798.png', 'test ', 'tarjeta', '2026-05-10', 'pagada', NULL, '2025-07-10 13:45:13', NULL, NULL, NULL, NULL, NULL),
-(18, 20, 'cobranza', 400.00, '76543', '/uploads/facturas/1752177133885-761981579.png', 'esa', '', '2025-07-12', 'pagada', 'Solicitud aprobada', '2025-07-10 13:52:13', 5, 6, '2025-07-10 23:02:02', '2025-07-10 23:22:43', NULL),
-(19, 20, 'comercial', 2000.00, '76543098765432198765432', '/uploads/facturas/1752185844451-591752214.pdf', '65432', 'nominas', '2025-07-12', 'pagada', 'Solicitud aprobada', '2025-07-10 13:54:44', 5, 6, '2025-07-10 23:02:17', '2025-07-10 23:39:24', NULL),
-(21, 25, 'ti', 500.00, '1234567890', '/uploads/facturas/1752203061957-298922629.jpg', 'TEST ', 'nominas', '2025-07-18', 'pendiente', NULL, '2025-07-10 21:03:03', NULL, NULL, NULL, NULL, NULL);
+(14, 20, 'ti', 2222222.00, '1234567890', '/uploads/facturas/1752173198561-209150711.png', '98765432', 'efectivo', '2025-08-09', 'pagada', 'bsb', '2025-07-10 12:46:38', 5, 22, '2025-07-11 11:25:30', '2025-07-11 11:26:49', NULL),
+(15, 20, 'vinculacion', 10033.00, '1234567890', '/uploads/facturas/1752173900916-983062405.png', 'test ', 'efectivo', '2025-08-09', 'autorizada', NULL, '2025-07-10 12:58:20', NULL, NULL, NULL, NULL, NULL),
+(17, 20, 'atencion a clientes', 1223344.00, '1234567890', '/uploads/facturas/1752176713500-269532798.png', 'test ', 'tarjeta', '2026-05-10', 'pendiente', NULL, '2025-07-10 13:45:13', NULL, NULL, NULL, NULL, NULL),
+(18, 20, 'cobranza', 400000.00, '76543098765432', '/uploads/facturas/1752177133885-761981579.png', 'esa', '', '2025-07-12', 'pagada', 'Solicitud aprobada', '2025-07-10 13:52:13', 23, 22, '2025-07-11 10:23:09', '2025-07-11 11:17:09', NULL),
+(19, 20, 'comercial', 2000.00, '76543098765432198765432', '/uploads/facturas/1752185844451-591752214.pdf', '65432', 'nominas', '2025-07-12', 'pendiente', NULL, '2025-07-10 13:54:44', NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -198,15 +196,23 @@ INSERT INTO `usuarios` (`id_usuario`, `nombre`, `email`, `password`, `rol`, `cre
 (3, 'Leonardo Admin', 'admin@bechapra.com', '$2b$10$d0r2Uhx4p2CXuns3ib6J/.uzbZHUxZ0db0vCqeSR5cN8H0pooorOu', 'admin_general', '2025-06-17 19:32:30', 0, 0, NULL, 0),
 (5, 'Aprobador Uno', 'aprobador@bechapra.com', '$2b$10$OFRyDdjyQlfAanLNzFXHR.mM/Tb6UCBp.q0xOujDbyDkurwhhcIqS', 'aprobador', '2025-06-17 19:32:30', 0, 0, NULL, 0),
 (6, 'Pagador Uno', 'pagador@bechapra.com', '$2b$10$GxEQD8KJE.7j6Qvsh.GQMOooJWXclLYwX.NL/mmDKOKXdlXUr36EW', 'pagador_banca', '2025-06-17 19:32:30', 0, 0, NULL, 0),
-(20, 'solicitante2', 'solicitante2@bechapra.com', '$2b$10$nr3BChbHInuAM8t2.7ppk.rz2i9XuVmYHdU5.1aQMGy0bGA5.LjUO', 'solicitante', '2025-07-08 19:13:17', 0, 0, NULL, 0),
-(22, 'panchito', 'panchito@bechapra.com', '$2b$10$ai3Ulwj/MwHo3zb8uPQyludVuzkEf1KtwvvurJkJ/.h4w1jIfGuV.', 'solicitante', '2025-07-08 22:38:13', 0, 0, NULL, 0),
-(23, 'luis enrike peña nito', 'kike@bechapra.com', '$2b$10$jJRguLAQGsKbJi8zfZFfKOyINTnQDHThxDDvarxWqonbnBoHdjI1i', 'solicitante', '2025-07-09 19:18:52', 0, 0, NULL, 0),
-(24, 'luis enrike peña nieoto', 'kiike@bechapra.com', '$2b$10$Jnl6nV/NeiQE0tnQ3G5zMe/O2XXoA0a0Gb4kAI7cZ75w6gdDHR0qG', 'solicitante', '2025-07-09 19:19:42', 0, 0, NULL, 0),
-(25, 'leonardo', 'leo@bechapra.com', '$2b$10$ym2ZU8v82RYrzdAEu3V2u.QMGP0oyZsmwMFGqd5xbp.vr4OC7taOO', 'solicitante', '2025-07-09 19:29:25', 0, 0, NULL, 0);
+(20, 'solicitante2', 'solicitante2@bechapra.com', '$2b$10$J0DnjcyEdvY7tUBpbIkOpu4V9hjxIsXgygOWeFw2Jj5fxSxA54Hl6', 'solicitante', '2025-07-08 19:13:17', 0, 0, NULL, 0),
+(22, 'panchito', 'panchito@bechapra.com', '$2b$10$w.qpewn5UbNoqKnQwCdD4OH6qylpXGTb3XtxtRm5JX1q7tzCaXi1q', 'pagador_banca', '2025-07-08 22:38:13', 0, 0, NULL, 0),
+(23, 'luis enrike peña nito', 'kike@bechapra.com', '$2b$10$P5pmeOyAXht/01OJXFLYfeFqM2YYacVtWprIz6oE3ErX3FQ5s3e8a', 'aprobador', '2025-07-09 19:18:52', 0, 0, NULL, 0),
+(24, 'luis enrike peña nieoto', 'kiike@bechapra.com', '$2b$10$ngy21wrY1fx1WPNmvDyRH.PTqQ3.8ES2PwNjXk2gswFkdn.6P4ZLW', 'pagador_banca', '2025-07-09 19:19:42', 0, 0, NULL, 0),
+(25, 'leonardo', 'leo@bechapra.com', '$2b$10$9ItkIc671diE144NI5Wwg.p4jzpxMD5bKnpSlVjrM73vBgOMsqY8.', 'aprobador', '2025-07-09 19:29:25', 0, 0, NULL, 0);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `comprobantes_pago`
+--
+ALTER TABLE `comprobantes_pago`
+  ADD PRIMARY KEY (`id_comprobante`),
+  ADD KEY `id_solicitud` (`id_solicitud`),
+  ADD KEY `usuario_subio` (`usuario_subio`);
 
 --
 -- Indices de la tabla `ejecuciones_recurrentes`
@@ -251,6 +257,12 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comprobantes_pago`
+--
+ALTER TABLE `comprobantes_pago`
+  MODIFY `id_comprobante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de la tabla `ejecuciones_recurrentes`
 --
 ALTER TABLE `ejecuciones_recurrentes`
@@ -260,7 +272,7 @@ ALTER TABLE `ejecuciones_recurrentes`
 -- AUTO_INCREMENT de la tabla `notificaciones`
 --
 ALTER TABLE `notificaciones`
-  MODIFY `id_notificacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_notificacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `pagos_recurrentes`
@@ -272,7 +284,7 @@ ALTER TABLE `pagos_recurrentes`
 -- AUTO_INCREMENT de la tabla `solicitudes_pago`
 --
 ALTER TABLE `solicitudes_pago`
-  MODIFY `id_solicitud` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_solicitud` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -283,6 +295,13 @@ ALTER TABLE `usuarios`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `comprobantes_pago`
+--
+ALTER TABLE `comprobantes_pago`
+  ADD CONSTRAINT `fk_comprobante_solicitud` FOREIGN KEY (`id_solicitud`) REFERENCES `solicitudes_pago` (`id_solicitud`),
+  ADD CONSTRAINT `fk_comprobante_usuario` FOREIGN KEY (`usuario_subio`) REFERENCES `usuarios` (`id_usuario`);
 
 --
 -- Filtros para la tabla `ejecuciones_recurrentes`
